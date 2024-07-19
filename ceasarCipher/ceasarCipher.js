60500c02087c712844f812e22e0428c3205bd6ee
xonin-hush <xonindev@gmail.com>
2024-07-19 20:36:21 +0100

    Add ceasarCipher

---
+++ ceasarCipher/ceasarCipher.js
@@ -0,0 +1,7 @@
+const caesarCipher = (str, shift) => {
+    if (shift === 0) return str;
+
+    return str.replace(/[a-z]/g, (char) =>
+        String.fromCharCode(((char.charCodeAt(0) - 97 + shift) % 26) + 97)
+    );
+};
